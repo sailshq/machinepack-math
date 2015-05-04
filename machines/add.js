424f701c73023b2646c16c1dc1ac8e585504ebdb
Mike McNeil <mike@balderdash.co>
2015-05-04 16:32:02 -0500

    Initial commit (includes 'add' machine)

---
+++ machines/add.js
@@ -0,0 +1,49 @@
+module.exports = {
+
+
+  friendlyName: 'Add (+)',
+
+
+  description: 'Add two numbers together and return the sum.',
+
+
+  sync: true,
+
+
+  cacheable: true,
+
+
+  inputs: {
+
+    a: {
+      description: 'The first number.',
+      example: 2,
+      required: true
+    },
+
+    b: {
+      description: 'The second number.',
+      example: 2.2,
+      required: true
+    }
+  },
+
+
+  exits: {
+
+    success: {
+      friendlyName: 'then',
+      description: 'Returns the sum (a + b)',
+      variableName: 'sum',
+      example: 4.2
+    }
+
+  },
+
+
+  fn: function(inputs, exits) {
+    return exits.success(inputs.a + inputs.b);
+  }
+
+
+};
